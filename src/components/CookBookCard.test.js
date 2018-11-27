cd5e3ff850c15b0c0ae3404a81a98e5839f3545f
aubreeabril <aubree.abril@gmail.com>
2018-11-27 00:18:51 -0500

    fixing cookbook test file

--- src/components/CookBookCard.test.js
+++ src/components/CookBookCard.test.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { shallow } from "enzyme";
 import renderer from "react-test-renderer";
-import CookBookCard from "./CookBookCard";
+import CookbookCard from "./CookBookCard";
 
 describe("CookBook Card", () => {
   it("renders the cookbook card", () => {
